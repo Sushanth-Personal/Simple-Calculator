9f5ca51977cfab5b343efee6ea408dba9af08771
Sushanth-Personal <168424396+Sushanth-Personal@users.noreply.github.com>
2024-08-15 12:40:56 +0530

    Added keyboard functionality

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let text = "";
 let resultClicked = false;
-let operandClicked=false;
+let operandClicked = false;
 
 function numberButton(button, data) {
   button.addEventListener("click", (event) => {
-    operandClicked=false;
+    operandClicked = false;
     if (resultClicked == true) {
       text = "";
       resultClicked = false;
@@ -17,19 +17,16 @@ function numberButton(button, data) {
 
 function operandButton(button, data) {
   button.addEventListener("click", (event) => {
-    if(operandClicked!=true){
-        
-    resultClicked = false;
-    text = text + data;
-    console.log(text);
-    selectID("expression").textContent = text;
+    if (operandClicked != true) {
+      resultClicked = false;
+      text = text + data;
+      selectID("expression").textContent = text;
+    } else {
+      selectID("delete").click();
+      text = text + data;
+      selectID("expression").textContent = text;
     }
-    else{
-        selectID("delete").click();
-        text=text+data;
-        selectID("expression").textContent=text;
-    }
-    operandClicked=true;
+    operandClicked = true;
   });
 }
 
@@ -53,7 +50,8 @@ operandButton(selectID("sub"), "-");
 operandButton(selectID("divide"), "/");
 numberButton(selectID("dec"), ".");
 
-selectID("delete").addEventListener("click", ()=>{
+selectID("delete").addEventListener("click", () => {
+  text = selectID("expression").textContent;
   text = text.slice(0, -1);
   document.getElementById("expression").textContent = text;
 });
@@ -72,4 +70,70 @@ selectID("result").addEventListener("click", () => {
 selectID("reset").addEventListener("click", () => {
   selectID("expression").textContent = "0";
   text = "";
+});
+
+document.addEventListener("keydown", (event) => {
+  const key = event.key;
+  switch (key) {
+    case "1":
+      selectID("1").click();
+      break;
+    case "2":
+      selectID("2").click();
+      break;
+    case "3":
+      selectID("3").click();
+      break;
+    case "4":
+      selectID("4").click();
+      break;
+    case "5":
+      selectID("5").click();
+      break;
+    case "6":
+      selectID("6").click();
+      break;
+    case "7":
+      selectID("7").click();
+      break;
+    case "8":
+      selectID("8").click();
+      break;
+    case "9":
+      selectID("9").click();
+      break;
+    case "0":
+      selectID("0").click();
+      break;
+    case "+":
+      selectID("add").click();
+      break;
+    case "-":
+      selectID("sub").click();
+      break;
+    case "/":
+      selectID("divide").click();
+      break;
+    case "*":
+      selectID("mul").click();
+      break;
+    case "x":
+      selectID("mul").click();
+      break;
+    case ".":
+      selectID("dec").click();
+      break;
+    case "=":
+      selectID("result").click();
+      break;
+    case "Enter":
+      selectID("result").click();
+      break;
+    case "Backspace":
+      selectID("delete").click();
+      break;
+    case "Delete":
+      selectID("delete").click();
+      break;
+  }
 });
